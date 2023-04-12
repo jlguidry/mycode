288edf3a75d2848c462bad49998944d706bedcde
jlguidry <jamesguidry85@gmail.com>
2023-04-12 15:49:15 +0000

    SimpsonsChallenge

---
+++ dict01/pythondictSimpson.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Simpsons challenge
+
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+
+print(f"My {challenge[2][1]}! The {challenge[2][0]} do {challenge[3]}!")
+
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}}, "banana": 15, "d": "nothing"}]
+
