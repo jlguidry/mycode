b79f64799a7f344430c85303b8762ea7a17a46ef
jlguidry <jamesguidry85@gmail.com>
2023-04-13 18:55:01 +0000

    vampytimex

---
+++ cfgread/looping_vampire.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Looping Vampires Challenge"""
+
+
+counter=0
+
+with open("dracula.txt","r") as foo:
+    with open("vampytimex.txt","w") as fang:
+        for line in foo:
+            if "vampire" in line.lower():
+                print(line)
+                counter += 1
+                fang.write(line)
+
+print(counter)
