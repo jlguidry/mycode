0710a8ea5ce5d94e0faaf0b12930c7dc833fcc08
jlguidry <jamesguidry85@gmail.com>
2023-04-14 14:50:25 +0000

    mini project

--- my_projects/mini_project/rockpaperscissors.py
+++ my_projects/mini_project/rockpaperscissors.py
@@ -7,46 +7,54 @@
 #Import modules
 import random
 
+import crayons
+
 def main():
     """Pylint really wanted a docstring here"""
 
 #Useless nonsense for my amusement
-    answer = input("\nHi! My name is Py. Do you want to play Rock Paper Scissors?\n").lower()
+    answer = input(crayons.yellow("\nHi! My name is Py. Do you want to play Rock Paper Scissors?\n")).lower()
 
     if answer in ["yes", "ye", "y"]:
-        print("\nYay!\n")
+        print(crayons.yellow("\nYay!\n"))
     elif answer in ["no", "n"]:
-        print("\nI'm not letting you leave until you play. :)\n")
+        print(crayons.red("\nI'm not letting you leave until you play. :)\n"))
     else:
-        print("\nI didn't understand your response.\nSo... We'll play anyway! :P\n")
+        print(crayons.cyan("\nI didn't understand your response.\nSo... We'll play anyway! :P\n"))
 
 #Acutual initial game input
-    player_choice = input("Rock Paper Scissors, What's your choice?").title()
+    player_choice = input(crayons.yellow("Rock Paper Scissors, What's your choice?\n\n>>>")).title()
 
     choices = ["Rock", "Paper", "Scissors"]
     computer_choice = random.choice(choices)
 
 #Let's see who our winner is
-    print(f"\n{player_choice} vs {computer_choice}\n")
+
+#couldn't use "\n" in an f string)
+
+    print("\n")
+    print(f"{crayons.red(player_choice)} vs {crayons.cyan(computer_choice)}")
+    print("\n")
 
     if player_choice == computer_choice:
-        print("It's Tie!")
+        print(crayons.blue("It's Tie!"))
     elif (player_choice == "Rock" and computer_choice == "Paper"):
-        print("Paper covers Rock. I win!")
+        print(crayons.blue("Paper covers Rock. I win!"))
     elif (player_choice == "Rock" and computer_choice == "Scissors"):
-        print("Rock breaks Scissors. You win!")
+        print(crayons.blue("Rock breaks Scissors. You win!"))
     elif (player_choice == "Paper" and computer_choice == "Rock"):
-        print("Paper covers Rock. You win!")
+        print(crayons.blue("Paper covers Rock. You win!"))
     elif (player_choice == "Paper" and computer_choice == "Scissors"):
-        print("Scissors cuts Paper. I win!")
+        print(crayons.blue("Scissors cuts Paper. I win!"))
     elif (player_choice == "Scissors" and computer_choice == "Rock"):
-        print("Rock breaks Scissors. I win!")
+        print(crayons.blue("Rock breaks Scissors. I win!"))
     elif (player_choice == "Scissors" and computer_choice == "Paper"):
-        print("Scissors cuts Paper. You win!")
+        print(crayons.blue("Scissors cuts Paper. You win!"))
     elif player_choice in ["Lizard", "Spock"]:
-        print("Nice try, but that's cheating. I win")
+        print(crayons.green("Nice try, but that's cheating. I win"))
     else:
-        print("I didn't understand you, so I win!!")
+        print(crayons.red("I didn't understand you, so I win!!"))
+    print("\n")    
 
 if __name__ == "__main__":
     main()
